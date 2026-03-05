c8034bfedaab8a421ecec65c1fce392e53fa3132
adityakumar <adityabrn2004@gmail.com>
2026-03-05 20:58:44 +0530

    update

---
+++ QuickSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int partition (int arr[], int s, int e) {
+    int pivot = arr[e];
+    int i = s-1;
+    for (int j=s; j<e; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1], arr[e]);
+    return i+1;
+}
+
+void quickSort (int arr[], int s, int e) {
+    if (s >= e) return;
+    int pi = partition (arr, s, e);
+    quickSort (arr, s, pi-1);
+    quickSort (arr, pi+1, e);
+}
+
+
+int main() {
+    int arr[] = {10,3,9,2,15};
+    int n = 5;
+    quickSort(arr, 0, n-1);
+    for (int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
