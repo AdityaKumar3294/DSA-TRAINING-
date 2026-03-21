0f72bc5a5c513e100173eff4c68bc86b3a8fa1b0
adityakumar <adityabrn2004@gmail.com>
2026-03-21 23:25:02 +0530

    update

--- doubly-LL.cpp
+++ doubly-LL.cpp
@@ -5,11 +5,39 @@ struct Node {
     int data;
     Node* next;
 };
- void insertAthead(Node* head, int val) {
+
+ void insertAthead(Node* &head, int val) {
     Node* newNode = new Node();
     newNode->data = val;
     newNode->next = head;
     head = newNode;
+ }
+
+ void insertAttail(Node* &head, int val) {
+    Node*newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+    while (head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+ }
+
+ void display(Node* head) {
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+    
  }
  int main() {
     Node* head = NULL;
