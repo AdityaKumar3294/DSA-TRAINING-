d2e3bc07ffabd96fb7b941ba8ea5e5c478dc36c0
adityakumar <adityabrn2004@gmail.com>
2026-04-04 22:33:26 +0530

    update

---
+++ leetcode40.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || left == right) return head;
+
+        // Step 1: Create dummy node
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+
+        // Step 2: Move prev to node before 'left'
+        for (int i = 1; i < left; i++) {
+            prev = prev->next;
+        }
+
+        // Step 3: Start reversing
+        ListNode* curr = prev->next;
+        ListNode* next = NULL;
+
+        for (int i = 0; i < right - left; i++) {
+            next = curr->next;
+            curr->next = next->next;
+            next->next = prev->next;
+            prev->next = next;
+        }
+
+        return dummy->next;
+    }
+};
